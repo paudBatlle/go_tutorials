9cde21d8ce50f35b04321aaf7ea51a21411c0ec1
paudBatlle <pau.debatlle01@estudiant.upf.edu>
2024-11-23 17:42:54 +0100

    Structs and Interfaces

--- cmd/tutorial_5/main.go
+++ cmd/tutorial_5/main.go
@@ -8,8 +8,33 @@ type gasEngine struct {
 	owner
 }
 
+type electrincEngine struct {
+	mpkwh uint8
+	kwh   uint8
+	owner
+}
+
 type owner struct {
 	name string
+}
+
+func (e gasEngine) milesLeft() uint8 {
+	return e.gallons * e.mpg
+}
+func (e electrincEngine) milesLeft() uint8 {
+	return e.kwh * e.mpkwh
+}
+
+type engine interface {
+	milesLeft() uint8
+}
+
+func canMakeIt(e engine, miles uint8) {
+	if miles <= e.milesLeft() {
+		fmt.Println("You can make it !")
+	} else {
+		fmt.Println("Need to refuel first")
+	}
 }
 
 // Structs and Interfaces
@@ -19,5 +44,6 @@ func main() {
 
 	var myEngine2 gasEngine = gasEngine{25, 15, owner{"Pau"}}
 	fmt.Println(myEngine2.mpg, myEngine2.gallons, myEngine2.name)
-
+	//fmt.Printf("Total miles left in tank: %v", myEngine2.milesLeft())
+	canMakeIt(myEngine2, 50)
 }
